879b213146b97d7b99cccfff644a214bf666114c
duelago <duelago@gmail.com>
2024-04-20 18:47:31 +0200

    Update lampa-megapol.ino
    Lagt till fält för låt på HTML-sidan
    åäö funkar inte längre

--- lampa-megapol.ino
+++ lampa-megapol.ino
@@ -20,7 +20,7 @@ Adafruit_SSD1306 display(OLED_RESET);
 #define PIN            D4  // Define the pin for WS2811 LEDs
 #define NUM_LEDS       1   // Define the number of LEDs
 #define LED_BRIGHTNESS 255  // Define LED brightness (0-255)
-#define EEPROM_SIZE    5   // Size of EEPROM for storing METAR station code (max 4 characters + null terminator)
+#define EEPROM_SIZE    50   // Size of EEPROM for storing METAR station code and song title
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 ESP8266WebServer server(80);
@@ -43,130 +43,31 @@ void handleJsonObject(JsonObject obj) {
     Serial.print("Track Title: ");
     Serial.println(songTitle);
     
-    // If the song title is "Last Christmas", start blinking the LED
-    if (songTitle == "Last Christmas") {
+    // Retrieve the song title from EEPROM
+    char storedSongTitle[EEPROM_SIZE];
+    EEPROM.begin(EEPROM_SIZE);
+    for (int i = 0; i < EEPROM_SIZE; ++i) {
+        storedSongTitle[i] = EEPROM.read(i);
+    }
+    EEPROM.end();
+    
+    // Convert the character array to String
+    String storedTitle = String(storedSongTitle);
+    
+    // If the retrieved song title matches the one from API, start blinking the LED
+    if (songTitle == storedTitle) {
         blinkLED = true;
     } else {
         blinkLED = false;
-        // Turn off the LED if the song title is not "Last Christmas"
+        // Turn off the LED if the song title doesn't match
         strip.setPixelColor(0, strip.Color(0, 0, 0));
         strip.show();
     }
 }
 
-String parseTemperature(const String& metar) {
-    // Find the position of "Temperature: " in the METAR string
-    int tempPos = metar.indexOf("Temperature: ");
-    if (tempPos != -1) {
-        // Extract the substring starting from "Temperature: "
-        String tempSubstring = metar.substring(tempPos);
-
-        // Find the position of "(" and ")" to extract the Celsius temperature
-        int openParenthesisPos = tempSubstring.indexOf('(');
-        int closeParenthesisPos = tempSubstring.indexOf(')');
-
-        if (openParenthesisPos != -1 && closeParenthesisPos != -1) {
-            // Extract the substring between "(" and ")"
-            String celsiusString = tempSubstring.substring(openParenthesisPos + 1, closeParenthesisPos);
-            return celsiusString;
-        }
-    }
-
-    // Return an empty string if parsing fails
-    return "";
-}
-
-void setLEDColor(float tempLed) {
-    uint32_t color = 0;
-
-    if (tempLed >= 0 && tempLed <= 5) {
-        color = strip.Color(255, 0, 0);
-    } else if (tempLed >= -5 && tempLed < 0) {
-        color = strip.Color(0, 0, 255);
-    } else if (tempLed >= -50 && tempLed < -5) {
-        color = strip.Color(0, 255, 255);
-    } else if (tempLed >= 6 && tempLed <= 10) {
-        color = strip.Color(255, 153, 0);
-    } else if (tempLed >= 11 && tempLed <= 45) {
-        color = strip.Color(0, 255, 0);
-    }
-
-    for (int i = 0; i < NUM_LEDS; i++) {
-        strip.setPixelColor(i, color);
-    }
-    strip.show();
-}
-
-void handleRoot() {
-    String html = "<html><head><style>";
-    html += "body { font-family: Arial, sans-serif; background-color: #f4f4f4; margin: 0; padding: 0; }";
-    html += ".container { max-width: 800px; margin: 0 auto; padding: 20px; background-color: #ffffff; box-shadow: 0 0 10px rgba(0,0,0,0.1); border-radius: 10px; margin-top: 20px; }";
-    html += ".input-group { margin-bottom: 20px; }";
-    html += ".input-group input[type='text'] { width: 80%; padding: 8px; font-size: 16px; border: 1px solid #ccc; border-radius: 5px; }";
-    html += ".input-group input[type='submit'] { width: 18%; padding: 8px; font-size: 16px; border: none; background-color: #4caf50; color: #ffffff; cursor: pointer; border-radius: 5px; }";
-    html += ".metar-info { font-size: 18px; margin-top: 20px; }";
-    html += ".update-link { text-decoration: none; color: #4caf50; font-weight: bold; margin-top: 20px; display: block; }";
-    html += "</style></head><body>";
-    html += "<div class='container'><h1>METAR-kod:</h1>";
-    html += "<div class='input-group'><form method='post' action='/submit'>";
-    html += "<input type='text' name='stationCode' placeholder='ESSB' />";
-
-    html += "<input type='submit' value='Skicka' />";
-    html += "</form>";
-    html += "<p>ESGG G&ouml;TEBORG/Landvetter<br>";
-    html += "ESOK KARLSTAD<br>";
-    html += "ESNQ KIRUNA<br>";
-    html += "ESMS MALM&ouml;<br>";
-    html += "ESSA STOCKHOLM/Arlanda<br>";
-    html += "ESSB STOCKHOLM/Bromma<br>";
-    html += "ESKN STOCKHOLM/Skavsta<br>";
-    html += "ESOW STOCKHOLM/V&auml;ster&aring;s<br>";
-    html += "<p>";
-    html += "ESNX Arvidsjaur<br>";
-    html += "ESSD Borl&auml;nge<br>";
-    html += "ESNG G&auml;llivare<br>";
-    html += "ESMT Halmstad (F14)<br>";
-    html += "ESUT Hemavan<br>";
-    html += "ESGJ J&ouml;nk&ouml;ping<br>";
-    html += "ESMQ Kalmar (F12)<br>";
-    html += "ESNK Kramfors/Sollefte&aring;<br>";
-    html += "ESMK Kristianstad/Ever&ouml;d<br>";
-    html += "ESCF Link&ouml;ping/Malmen (F3)<br>";
-    html += "ESTL Ljungbyhed (F5)<br>";
-    html += "ESPA Lule&aring; (F21)<br>";
-    html += "ESNL Lycksele<br>";
-    html += "ESSP Norrk&ouml;ping/Kungs&auml;ngen<br>";
-    html += "ESUP Pajala<br>";
-    html += "ESDF Ronneby (F17)<br>";
-    html += "ESNS Skellefte&aring;<br>";
-    html += "ESGR Sk&ouml;vde<br>";
-    html += "ESNN Sundsvall-Timr&aring;<br>";
-    html += "ESIB S&aring;ten&auml;s (F7)<br>";
-    html += "ESKS S&auml;len<br>";
-    html += "ESGT Trollh&auml;ttan-V&auml;nersborg<br>";
-    html += "ESNU Ume&aring;<br>";
-    html += "ESNV Vilhelmina<br>";
-    html += "ESSV Visby<br>";
-    html += "ESMX V&auml;xj&ouml;/Kronoberg<br>";
-    html += "ESTA &Auml;ngelholm/Bark&aring;kra (F10)<br>";
-    html += "ESOE &Ouml;rebro<br>";
-    html += "ESNO &Ouml;rnsk&ouml;ldsvik/Gide&aring;<br>";
-    html += "ESNZ &Ouml;stersund/Fr&ouml;s&ouml;n (F4)";
-    html += "</div>";
-    html += "<div class='metar-info'>Senaste METAR:<br>";
-    html += METAR;
-    html += "<p>Song Title: ";
-    html += songTitle;
-    html += "<p><a href='/update'>Firmwareuppdatering</a></p><br>";
-    html += "Version 0.9 Temperaturlampan<br>";
-    html += "</div>";
-    html += "</body></html>";
-
-    server.send(200, "text/html", html);
-}
-
 void handleSubmit() {
     String stationCode = server.arg("stationCode");
+    String userSongTitle = server.arg("songTitle"); // Get user input for song title
 
     // Convert the input station code to uppercase
     stationCode.toUpperCase();
@@ -179,6 +80,12 @@ void handleSubmit() {
     for (int i = 0; i < EEPROM_SIZE; ++i) {
         EEPROM.write(i, METARStation[i]);
     }
+    
+    // Update EEPROM with the user-defined song title
+    for (int i = 0; i < userSongTitle.length(); ++i) {
+        EEPROM.write(EEPROM_SIZE + i, userSongTitle[i]);
+    }
+    
     EEPROM.end();
 
     server.send(200, "text/html", "Stationen uppdaterad. <a href='/'>Tillbaka</a>");
@@ -354,4 +261,120 @@ void loop() {
     }
 
     delay(100); // Delay for a short time to prevent excessive loop iterations
+}
+
+String parseTemperature(const String& metar) {
+    // Find the position of "Temperature: " in the METAR string
+    int tempPos = metar.indexOf("Temperature: ");
+    if (tempPos != -1) {
+        // Extract the substring starting from "Temperature: "
+        String tempSubstring = metar.substring(tempPos);
+
+        // Find the position of "(" and ")" to extract the Celsius temperature
+        int openParenthesisPos = tempSubstring.indexOf('(');
+        int closeParenthesisPos = tempSubstring.indexOf(')');
+
+        if (openParenthesisPos != -1 && closeParenthesisPos != -1) {
+            // Extract the substring between "(" and ")"
+            String celsiusString = tempSubstring.substring(openParenthesisPos + 1, closeParenthesisPos);
+            return celsiusString;
+        }
+    }
+
+    // Return an empty string if parsing fails
+    return "";
+}
+
+void setLEDColor(float tempLed) {
+    uint32_t color = 0;
+
+    if (tempLed >= 0 && tempLed <= 5) {
+        color = strip.Color(255, 0, 0);
+    } else if (tempLed >= -5 && tempLed < 0) {
+        color = strip.Color(0, 0, 255);
+    } else if (tempLed >= -50 && tempLed < -5) {
+        color = strip.Color(0, 255, 255);
+    } else if (tempLed >= 6 && tempLed <= 10) {
+        color = strip.Color(255, 153, 0);
+    } else if (tempLed >= 11 && tempLed <= 45) {
+        color = strip.Color(0, 255, 0);
+    }
+
+    for (int i = 0; i < NUM_LEDS; i++) {
+        strip.setPixelColor(i, color);
+    }
+    strip.show();
+}
+
+void handleRoot() {
+    String html = "<html><head><style>";
+    html += "body { font-family: Arial, sans-serif; background-color: #f4f4f4; margin: 0; padding: 0; }";
+    html += ".container { max-width: 800px; margin: 0 auto; padding: 20px; background-color: #ffffff; box-shadow: 0 0 10px rgba(0,0,0,0.1); border-radius: 10px; margin-top: 20px; }";
+    html += ".input-group { margin-bottom: 20px; }";
+    html += ".input-group input[type='text'] { width: 80%; padding: 8px; font-size: 16px; border: 1px solid #ccc; border-radius: 5px; }";
+    html += ".input-group input[type='submit'] { width: 18%; padding: 8px; font-size: 16px; border: none; background-color: #4caf50; color: #ffffff; cursor: pointer; border-radius: 5px; }";
+    html += ".metar-info { font-size: 18px; margin-top: 20px; }";
+    html += ".update-link { text-decoration: none; color: #4caf50; font-weight: bold; margin-top: 20px; display: block; }";
+    html += "</style></head><body>";
+    html += "<div class='container'><h1>METAR-kod:</h1>";
+    html += "<div class='input-group'><form method='post' action='/submit'>";
+    html += "<input type='text' name='stationCode' placeholder='ESSB' />";
+
+    // Add input field for song title
+    html += "<input type='text' name='songTitle' placeholder='Song Title' />";
+
+    html += "<input type='submit' value='Skicka' />";
+    html += "</form>";
+    html += "<p>ESGG GöTEBORG/Landvetter<br>";
+    html += "ESOK KARLSTAD<br>";
+    html += "ESNQ KIRUNA<br>";
+    html += "ESMS MALMÖ<br>";
+    html += "ESSA STOCKHOLM/Arlanda<br>";
+    html += "ESSB STOCKHOLM/Bromma<br>";
+    html += "ESKN STOCKHOLM/Skavsta<br>";
+    html += "ESOW STOCKHOLM/VäSTERåS<br>";
+    html += "<p>";
+    html += "ESNX Arvidsjaur<br>";
+    html += "ESSD Borlänge<br>";
+    html += "ESNG GäLLIVARE<br>";
+    html += "ESMT Halmstad (F14)<br>";
+    html += "ESUT Hemavan<br>";
+    html += "ESGJ JöNKöPING<br>";
+    html += "ESMQ Kalmar (F12)<br>";
+    html += "ESNK Kramfors/Sollefteå<br>";
+    html += "ESMK Kristianstad/EveröD<br>";
+    html += "ESCF LinköPING/Malmen (F3)<br>";
+    html += "ESTL Ljungbyhed (F5)<br>";
+    html += "ESPA Luleå (F21)<br>";
+    html += "ESNL Lycksele<br>";
+    html += "ESSP NorrköPING/KungsäNGEN<br>";
+    html += "ESUP Pajala<br>";
+    html += "ESDF Ronneby (F17)<br>";
+    html += "ESNS Skellefteå<br>";
+    html += "ESGR SköVde<br>";
+    html += "ESNN Sundsvall-Timrå<br>";
+    html += "ESIB SäTENäS (F7)<br>";
+    html += "ESKS SäLEN<br>";
+    html += "ESGT Trollhättan-VäNersborg<br>";
+    html += "ESNU UMEå<br>";
+    html += "ESNV Vilhelmina<br>";
+    html += "ESSV Visby<br>";
+    html += "ESMX VäXJö/Kronoberg<br>";
+    html += "ESTA ängelholm/BarkåKra (F10)<br>";
+    html += "ESOE öRebro<br>";
+    html += "ESNO öRnsköLdsvik/Gideå<br>";
+    html += "ESNZ öStersund/FröSöN";
+    html += "</div>";
+    html += "<div class='metar-info'>Senaste METAR:<br>";
+    html += METAR;
+    html += "<p>Song Title: ";
+    html += songTitle;
+    html += "<p><a href='/update'>Firmwareuppdatering</a></p><br>";
+    html += "Version 0.9 Temperaturlampan<br>";
+    html += "</div>";
+    html += "</body></html>";
+
+
+
+    server.send(200, "text/html", html);
 }

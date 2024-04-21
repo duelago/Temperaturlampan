abcc6ab00a69e7ba3e68ebd477d8acaa6f5aac75
duelago <duelago@gmail.com>
2024-04-21 08:13:00 +0200

    Update whamageddonlampan.ino
    Debuggar. Låten sätts till true men lampan blinkar inte

--- whamageddonlampan.ino
+++ whamageddonlampan.ino
@@ -20,7 +20,7 @@ Adafruit_SSD1306 display(OLED_RESET);
 #define PIN            D4  // Define the pin for WS2811 LEDs
 #define NUM_LEDS       1   // Define the number of LEDs
 #define LED_BRIGHTNESS 255  // Define LED brightness (0-255)
-#define EEPROM_SIZE    100   // Size of EEPROM for storing METAR station code and song title
+#define EEPROM_SIZE    5   // Size of EEPROM for storing METAR station code (max 4 characters + null terminator)
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 ESP8266WebServer server(80);
@@ -35,42 +35,141 @@ unsigned long previousMillis = 0;
 const char* apiUrl = "https://listenapi.planetradio.co.uk/api9.2/nowplaying/mme";
 
 bool blinkLED = false; // Flag to control LED blinking
-uint32_t previousColor = strip.Color(0, 0, 0); // Variable to store the previous LED color
 
 // Callback function to handle the JSON object
 void handleJsonObject(JsonObject obj) {
     const char* trackTitle = obj["TrackTitle"];
     songTitle = trackTitle;
-    Serial.print("Track Title: ");
+    Serial.print("Received Track Title: ");
     Serial.println(songTitle);
-    
-    // Retrieve the song title from EEPROM
-    char storedSongTitle[EEPROM_SIZE];
-    EEPROM.begin(EEPROM_SIZE);
-    for (int i = 0; i < EEPROM_SIZE; ++i) {
-        storedSongTitle[i] = EEPROM.read(i + EEPROM_SIZE); // Hämta sparad titel
-    }
-    EEPROM.end();
-    
-    // Convert the character array to String
-    String storedTitle = String(storedSongTitle);
-    
-    // If the retrieved song title matches the one from API, set the blinkLED flag
-    if (songTitle.equals(storedTitle)) {
+
+    // If the song title is "Mamma", start blinking the LED
+    if (songTitle == "Mamma") {
+        Serial.println("Song is Mamma. Setting blinkLED flag to true.");
         blinkLED = true;
     } else {
+        // Turn off the LED if the song title is not "Mamma"
+        Serial.println("Song is not Mamma. Keeping blinkLED flag false.");
         blinkLED = false;
-        // Restore previous LED color
-        for (int i = 0; i < NUM_LEDS; i++) {
-            strip.setPixelColor(i, previousColor);
-        }
+        strip.setPixelColor(0, strip.Color(0, 0, 0));
         strip.show();
     }
 }
 
+
+String parseTemperature(const String& metar) {
+    // Find the position of "Temperature: " in the METAR string
+    int tempPos = metar.indexOf("Temperature: ");
+    if (tempPos != -1) {
+        // Extract the substring starting from "Temperature: "
+        String tempSubstring = metar.substring(tempPos);
+
+        // Find the position of "(" and ")" to extract the Celsius temperature
+        int openParenthesisPos = tempSubstring.indexOf('(');
+        int closeParenthesisPos = tempSubstring.indexOf(')');
+
+        if (openParenthesisPos != -1 && closeParenthesisPos != -1) {
+            // Extract the substring between "(" and ")"
+            String celsiusString = tempSubstring.substring(openParenthesisPos + 1, closeParenthesisPos);
+            return celsiusString;
+        }
+    }
+
+    // Return an empty string if parsing fails
+    return "";
+}
+
+void setLEDColor(float tempLed) {
+    uint32_t color = 0;
+
+    if (tempLed >= 0 && tempLed <= 5) {
+        color = strip.Color(255, 0, 0);
+    } else if (tempLed >= -5 && tempLed < 0) {
+        color = strip.Color(0, 0, 255);
+    } else if (tempLed >= -50 && tempLed < -5) {
+        color = strip.Color(0, 255, 255);
+    } else if (tempLed >= 6 && tempLed <= 10) {
+        color = strip.Color(255, 153, 0);
+    } else if (tempLed >= 11 && tempLed <= 45) {
+        color = strip.Color(0, 255, 0);
+    }
+
+    for (int i = 0; i < NUM_LEDS; i++) {
+        strip.setPixelColor(i, color);
+    }
+    strip.show();
+}
+
+void handleRoot() {
+    String html = "<html><head><style>";
+    html += "body { font-family: Arial, sans-serif; background-color: #f4f4f4; margin: 0; padding: 0; }";
+    html += ".container { max-width: 800px; margin: 0 auto; padding: 20px; background-color: #ffffff; box-shadow: 0 0 10px rgba(0,0,0,0.1); border-radius: 10px; margin-top: 20px; }";
+    html += ".input-group { margin-bottom: 20px; }";
+    html += ".input-group input[type='text'] { width: 80%; padding: 8px; font-size: 16px; border: 1px solid #ccc; border-radius: 5px; }";
+    html += ".input-group input[type='submit'] { width: 18%; padding: 8px; font-size: 16px; border: none; background-color: #4caf50; color: #ffffff; cursor: pointer; border-radius: 5px; }";
+    html += ".metar-info { font-size: 18px; margin-top: 20px; }";
+    html += ".update-link { text-decoration: none; color: #4caf50; font-weight: bold; margin-top: 20px; display: block; }";
+    html += "</style></head><body>";
+    html += "<div class='container'><h1>METAR-kod:</h1>";
+    html += "<div class='input-group'><form method='post' action='/submit'>";
+    html += "<input type='text' name='stationCode' placeholder='ESSB' />";
+
+    html += "<input type='submit' value='Skicka' />";
+    html += "</form>";
+    html += "<p>ESGG G&ouml;TEBORG/Landvetter<br>";
+    html += "ESOK KARLSTAD<br>";
+    html += "ESNQ KIRUNA<br>";
+    html += "ESMS MALM&ouml;<br>";
+    html += "ESSA STOCKHOLM/Arlanda<br>";
+    html += "ESSB STOCKHOLM/Bromma<br>";
+    html += "ESKN STOCKHOLM/Skavsta<br>";
+    html += "ESOW STOCKHOLM/V&auml;ster&aring;s<br>";
+    html += "<p>";
+    html += "ESNX Arvidsjaur<br>";
+    html += "ESSD Borl&auml;nge<br>";
+    html += "ESNG G&auml;llivare<br>";
+    html += "ESMT Halmstad (F14)<br>";
+    html += "ESUT Hemavan<br>";
+    html += "ESGJ J&ouml;nk&ouml;ping<br>";
+    html += "ESMQ Kalmar (F12)<br>";
+    html += "ESNK Kramfors/Sollefte&aring;<br>";
+    html += "ESMK Kristianstad/Ever&ouml;d<br>";
+    html += "ESCF Link&ouml;ping/Malmen (F3)<br>";
+    html += "ESTL Ljungbyhed (F5)<br>";
+    html += "ESPA Lule&aring; (F21)<br>";
+    html += "ESNL Lycksele<br>";
+    html += "ESSP Norrk&ouml;ping/Kungs&auml;ngen<br>";
+    html += "ESUP Pajala<br>";
+    html += "ESDF Ronneby (F17)<br>";
+    html += "ESNS Skellefte&aring;<br>";
+    html += "ESGR Sk&ouml;vde<br>";
+    html += "ESNN Sundsvall-Timr&aring;<br>";
+    html += "ESIB S&aring;ten&auml;s (F7)<br>";
+    html += "ESKS S&auml;len<br>";
+    html += "ESGT Trollh&auml;ttan-V&auml;nersborg<br>";
+    html += "ESNU Ume&aring;<br>";
+    html += "ESNV Vilhelmina<br>";
+    html += "ESSV Visby<br>";
+    html += "ESMX V&auml;xj&ouml;/Kronoberg<br>";
+    html += "ESTA &Auml;ngelholm/Bark&aring;kra (F10)<br>";
+    html += "ESOE &Ouml;rebro<br>";
+    html += "ESNO &Ouml;rnsk&ouml;ldsvik/Gide&aring;<br>";
+    html += "ESNZ &Ouml;stersund/Fr&ouml;s&ouml;n (F4)";
+    html += "</div>";
+    html += "<div class='metar-info'>Senaste METAR:<br>";
+    html += METAR;
+    html += "<p>Song Title: ";
+    html += songTitle;
+    html += "<p><a href='/update'>Firmwareuppdatering</a></p><br>";
+    html += "Version 0.9 Temperaturlampan<br>";
+    html += "</div>";
+    html += "</body></html>";
+
+    server.send(200, "text/html", html);
+}
+
 void handleSubmit() {
     String stationCode = server.arg("stationCode");
-    String userSongTitle = server.arg("songTitle"); // Get user input for song title
 
     // Convert the input station code to uppercase
     stationCode.toUpperCase();
@@ -83,17 +182,10 @@ void handleSubmit() {
     for (int i = 0; i < EEPROM_SIZE; ++i) {
         EEPROM.write(i, METARStation[i]);
     }
-    
-    // Update EEPROM with the user-defined song title
-    for (int i = 0; i < userSongTitle.length(); ++i) {
-        EEPROM.write(i + EEPROM_SIZE, userSongTitle[i]); // Skriv till EEPROM från rätt plats
-    }
-    
     EEPROM.end();
 
     server.send(200, "text/html", "Stationen uppdaterad. <a href='/'>Tillbaka</a>");
 }
-
 
 void fetchMETARData() {
     // Fetch METAR data using METARStation variable (e.g., "ESSA" or user-defined value)
@@ -146,7 +238,7 @@ void setup() {
     delay(2000);
     display.clearDisplay();
     Serial.begin(115200);
-    MDNS.begin("whamageddonlampan");
+    MDNS.begin("temperaturlampan");
     ElegantOTA.begin(&server);
 
     // Initialize NeoPixel strip
@@ -155,7 +247,7 @@ void setup() {
 
     // Use WiFiManager to set WiFi credentials if they are not already configured
     WiFiManager wifiManager;
-    wifiManager.autoConnect("Whamageddonlampan");
+    wifiManager.autoConnect("Temperaturlampan");
 
     // Load METAR station code from EEPROM
     EEPROM.begin(EEPROM_SIZE);
@@ -212,6 +304,10 @@ void loop() {
             Serial.print("Sending HTTP request to: ");
             Serial.println(apiUrl);
 
+            // Save LED state before sending the request
+            uint32_t previousColor = strip.getPixelColor(0);
+            bool previousBlinkState = blinkLED;
+
             if (https.begin(client, apiUrl)) {
                 int httpCode = https.GET();
 
@@ -245,6 +341,11 @@ void loop() {
             } else {
                 Serial.println("Unable to connect");
             }
+
+            // Restore LED state after the request
+            strip.setPixelColor(0, previousColor);
+            blinkLED = previousBlinkState;
+            strip.show();
         } else {
             Serial.println("WiFi Disconnected. Reconnecting...");
             WiFiManager wifiManager;
@@ -253,164 +354,36 @@ void loop() {
         }
     }
 
-    // Call handleLED to control the LED
-    handleLED();
+    // Debugging output for blinkLED flag
+    //Serial.print("Before LED blinking: blinkLED flag: ");
+    //Serial.println(blinkLED);
+
+   // Blink LED if the flag is true
+if (blinkLED) {
+  // Blink LED
+  unsigned long blinkInterval = 500; // Blink interval in milliseconds
+  static bool ledState = false;
+  static unsigned long previousBlinkMillis = 0;
+
+  if (currentMillis - previousBlinkMillis >= blinkInterval) {
+    previousBlinkMillis = currentMillis;
+    ledState = !ledState;
+
+    // Set LED color based on ledState
+    strip.setPixelColor(0, ledState ? strip.Color(255, 255, 255) : strip.Color(0, 0, 0));
+
+    // Print debug info about LED state (optional)
+    Serial.print("LED state: ");
+    Serial.println(ledState ? "On (White)" : "Off (Black)");
+
+    strip.show();
+  }
+}
+
+    // Debugging output for blinkLED flag after LED blinking
+    //Serial.print("After LED blinking: blinkLED flag: ");
+    //Serial.println(blinkLED);
 
     delay(100); // Delay for a short time to prevent excessive loop iterations
 }
 
-String parseTemperature(const String& metar) {
-    // Find the position of "Temperature: " in the METAR string
-    int tempPos = metar.indexOf("Temperature: ");
-    if (tempPos != -1) {
-        // Extract the substring starting from "Temperature: "
-        String tempSubstring = metar.substring(tempPos);
-
-        // Find the position of "(" and ")" to extract the Celsius temperature
-        int openParenthesisPos = tempSubstring.indexOf('(');
-        int closeParenthesisPos = tempSubstring.indexOf(')');
-
-        if (openParenthesisPos != -1 && closeParenthesisPos != -1) {
-            // Extract the substring between "(" and ")"
-            String celsiusString = tempSubstring.substring(openParenthesisPos + 1, closeParenthesisPos);
-            return celsiusString;
-        }
-    }
-
-    // Return an empty string if parsing fails
-    return "";
-}
-
-void setLEDColor(float tempLed) {
-    uint32_t color = 0;
-
-    if (tempLed >= 0 && tempLed <= 5) {
-        color = strip.Color(255, 0, 0);
-    } else if (tempLed >= -5 && tempLed < 0) {
-        color = strip.Color(0, 0, 255);
-    } else if (tempLed >= -50 && tempLed < -5) {
-        color = strip.Color(0, 255, 255);
-    } else if (tempLed >= 6 && tempLed <= 10) {
-        color = strip.Color(255, 153, 0);
-    } else if (tempLed >= 11 && tempLed <= 45) {
-        color = strip.Color(0, 255, 0);
-    }
-
-    // Store the current LED color
-    previousColor = color;
-
-    for (int i = 0; i < NUM_LEDS; i++) {
-        strip.setPixelColor(i, color);
-    }
-    strip.show();
-}
-
-void handleRoot() {
-    // Load METAR station code from EEPROM
-    EEPROM.begin(EEPROM_SIZE);
-    char storedSongTitle[EEPROM_SIZE];
-    for (int i = 0; i < EEPROM_SIZE; ++i) {
-        storedSongTitle[i] = EEPROM.read(i + EEPROM_SIZE); // Hämta sparad titel
-    }
-    EEPROM.end();
-    
-    String html = "<html><head><meta charset='utf-8'><style>";
-    html += "body { font-family: Arial, sans-serif; background-color: #f4f4f4; margin: 0; padding: 0; }";
-    html += ".container { max-width: 800px; margin: 0 auto; padding: 20px; background-color: #ffffff; box-shadow: 0 0 10px rgba(0,0,0,0.1); border-radius: 10px; margin-top: 20px; }";
-    html += ".input-group { margin-bottom: 20px; }";
-    html += ".input-group input[type='text'] { width: 80%; padding: 8px; font-size: 16px; border: 1px solid #ccc; border-radius: 5px; }";
-    html += ".input-group input[type='submit'] { width: 18%; padding: 8px; font-size: 16px; border: none; background-color: #4caf50; color: #ffffff; cursor: pointer; border-radius: 5px; }";
-    html += ".metar-info { font-size: 18px; margin-top: 20px; }";
-    html += ".update-link { text-decoration: none; color: #4caf50; font-weight: bold; margin-top: 20px; display: block; }";
-    html += "</style></head><body>";
-    html += "<div class='container'><h1>Whamageddonlampan - METAR och Låttitel</h1>";
-    html += "<div class='input-group'><form method='post' action='/submit'>";
-    html += "<input type='text' name='stationCode' placeholder='ESSB' />";
-
-    // Add input field for song title
-    html += "<input type='text' name='songTitle' placeholder='Last Christmas' />";
-
-    html += "<input type='submit' value='Skicka' />";
-    html += "</form>";
-    html += "<p>ESGG GÖTEBORG/Landvetter<br>";
-    html += "ESOK KARLSTAD<br>";
-    html += "ESNQ KIRUNA<br>";
-    html += "ESMS MALMÖ<br>";
-    html += "ESSA STOCKHOLM/Arlanda<br>";
-    html += "ESSB STOCKHOLM/Bromma<br>";
-    html += "ESKN STOCKHOLM/Skavsta<br>";
-    html += "ESOW STOCKHOLM/Västerås<br>";
-    html += "<p>";
-    html += "ESNX Arvidsjaur<br>";
-    html += "ESSD Borlänge<br>";
-    html += "ESNG Gällivare<br>";
-    html += "ESMT Halmstad (F14)<br>";
-    html += "ESUT Hemavan<br>";
-    html += "ESGJ Jönköping<br>";
-    html += "ESMQ Kalmar (F12)<br>";
-    html += "ESNK Kramfors/Sollefteå<br>";
-    html += "ESMK Kristianstad/Everöd<br>";
-    html += "ESCF Linköping/Malmen (F3)<br>";
-    html += "ESTL Ljungbyhed (F5)<br>";
-    html += "ESPA Luleå (F21)<br>";
-    html += "ESNL Lycksele<br>";
-    html += "ESSP Norrköping/Kungsängen<br>";
-    html += "ESUP Pajala<br>";
-    html += "ESDF Ronneby (F17)<br>";
-    html += "ESNS Skellefteå<br>";
-    html += "ESGR Skövde<br>";
-    html += "ESNN Sundsvall-Timrå<br>";
-    html += "ESIB Såtenäs (F7)<br>";
-    html += "ESKS Sälen<br>";
-    html += "ESGT Trollhättan-Vänersborg<br>";
-    html += "ESNU Umeå<br>";
-    html += "ESNV Vilhelmina<br>";
-    html += "ESSV Visby<br>";
-    html += "ESMX Växjö/Kronoberg<br>";
-    html += "ESTA Ängelholm/Barkåkra (F10)<br>";
-    html += "ESOE ÖRebro<br>";
-    html += "ESNO Örnsköldsvik/Gideå<br>";
-    html += "ESNZ ÖStersund/Frösön";
-    html += "</div>";
-    html += "<div class='metar-info'>Senaste METAR:<br>";
-    html += METAR;
-    html += "<p>Sångtitel: ";
-    html += songTitle;
-    html += "<p>Sparad titel: ";
-    html += storedSongTitle;
-    html += "<p><a href='/update'>Firmwareuppdatering</a></p><br>";
-    html += "Version 0.1 Whamageddonlampan<br>";
-    html += "</div>";
-    html += "</body></html>";
-
-    server.send(200, "text/html", html);
-}
-
-
-// New function to control LED based on the blinkLED flag
-void handleLED() {
-    if (blinkLED) {
-        unsigned long currentMillis = millis();
-        static unsigned long previousMillis = 0;
-        static bool ledState = false;
-        const unsigned long interval = 500; // Interval in milliseconds
-        
-        if (currentMillis - previousMillis >= interval) {
-            previousMillis = currentMillis;
-            ledState = !ledState; // Toggle LED state
-            
-            if (ledState) {
-                // Turn on the LED
-                for (int i = 0; i < NUM_LEDS; i++) {
-                    strip.setPixelColor(i, previousColor);
-                }
-            } else {
-                // Turn off the LED
-                for (int i = 0; i < NUM_LEDS; i++) {
-                    strip.setPixelColor(i, strip.Color(0, 0, 0));
-                }
-            }
-            strip.show(); // Update LED
-        }
-    }
-}

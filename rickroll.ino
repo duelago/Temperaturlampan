ad881599918fdb4a79c1fc0722395a54e0170337
duelago <duelago@gmail.com>
2024-05-22 11:46:39 +0200

    Create rickroll.ino
    Buzzerljud för Never gonna give you up om man tröttnar på Last Christmas eller SM i Bangolf 2012 i Kalmar

---
+++ rickroll.ino
@@ -0,0 +1,254 @@
+// Never gonna give you up - buzzer
+
+void song(int buzzerPin){
+  
+  
+  tone(buzzerPin, 208);
+  delay(77);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 233);
+  delay(58);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 277);
+  delay(79);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 233);
+  delay(52);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 349);
+  delay(285);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 349);
+  delay(277);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 311);
+  delay(439);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 208);
+  delay(74);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 233);
+  delay(52);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 277);
+  delay(82);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 233);
+  delay(52);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 311);
+  delay(312);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 311);
+  delay(280);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 277);
+  delay(299);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 262);
+  delay(77);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 233);
+  delay(217);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 208);
+  delay(66);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 233);
+  delay(55);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 277);
+  delay(77);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 233);
+  delay(49);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 277);
+  delay(419);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 311);
+  delay(112);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 262);
+  delay(334);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 233);
+  delay(60);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 208);
+  delay(414);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 208);
+  delay(121);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 311);
+  delay(236);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 277);
+  delay(195);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 277);
+  delay(482);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 208);
+  delay(69);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 233);
+  delay(58);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 277);
+  delay(82);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 233);
+  delay(52);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 349);
+  delay(439);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 349);
+  delay(178);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 311);
+  delay(491);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 208);
+  delay(69);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 233);
+  delay(49);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 277);
+  delay(85);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 233);
+  delay(25);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 415);
+  delay(455);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 277);
+  delay(88);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 277);
+  delay(376);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 262);
+  delay(66);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 233);
+  delay(214);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 208);
+  delay(66);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 233);
+  delay(41);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 277);
+  delay(96);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 233);
+  delay(41);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 277);
+  delay(452);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 311);
+  delay(93);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 233);
+  delay(112);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 262);
+  delay(359);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 233);
+  delay(58);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 208);
+  delay(425);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 208);
+  delay(110);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 311);
+  delay(447);
+  noTone(buzzerPin);
+
+  tone(buzzerPin, 277);
+  delay(576);
+  noTone(buzzerPin);
+
+
+
+
+
+
+
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  // call the song function with digital pin
+  song(14);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
